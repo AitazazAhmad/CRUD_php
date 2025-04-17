062ffb5edbdca92a5cf036775ecbb361238b7bfe
AitazazAhmad <aitazazahmad007@gamil.com>
2025-04-17 18:13:48 +0500

    insert post method

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php include('header.php') ?>
 <?php include('dbcon.php') ?>
+<?php include('insert_data.php') ?>
 <div class="box1">
             <h2>All Students</h2>
-        <button  class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">>ADD STUDENTS</button>
+        <button  class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">ADD STUDENTS</button>
         </div>
     <table class="table table-hover table-bordered table-striped">
         <thead>
@@ -42,23 +43,48 @@
             </tbody>
         </thead>
     </table>
+    <?php
+  if(isset($_GET['message']))
+  {
+    echo "<h6>" .$_GET['message']. "</h6>";
+  }
+    ?>
+    <?php
+  if(isset($_GET['insert_msg']))
+  {
+    echo "<h6>" .$_GET['message']. "</h6>";
+  }
+    ?>
+    <form action="insert_data.php" method = "post">
     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <h1 class="modal-title fs-5" id="exampleModalLabel">ADD STUDENTS</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
+       </div>
+        <div class="form-group">
+          <label for="f_name">First Name</label>
+          <input type="text" name="f_name" class="form-control">
+        </div>
+        <div class="form-group">
+          <label for="f_name">Last Name</label>
+          <input type="text" name="l_name" class="form-control">
+        </div>
+        <div class="form-group">
+          <label for="f_name">Age</label>
+          <input type="text" name="age" class="form-control">
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+            <input type="submit" class="btn btn-success" name="add_students" value="ADD">
+        </div>
     </div>
-  </div>
 </div>
+</div>
+</form>
     <?php include('footer.php') ?>
 
    
